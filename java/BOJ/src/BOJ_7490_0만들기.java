c6327a7d857bcb9ba555c2c7e35aa4c924495de8
kimhyomin97 <rlagyalsdlek@naver.com>
2025-02-13 21:47:20 +0900

    BOJ DFS + 구현(스택)

--- java/BOJ/src/BOJ_7490_0만들기.java
+++ java/BOJ/src/BOJ_7490_0만들기.java
@@ -1,34 +1,58 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Stack;
 
 public class BOJ_7490_0만들기 {
-	static StringBuilder sb = new StringBuilder();
-	
-	static void recur(int now, int len, int sum, String temp, int before) {
-		if(now > len) {
-			if(sum == 0) sb.append(temp+"\n");
-//			System.out.println(temp + " : " + sum);
-			return;
-		}
-		recur(now+1, len, sum+now, temp+"+"+Integer.toString(now), now); // +
-		recur(now+1, len, sum-now, temp+"-"+Integer.toString(now), now*-1); // -
-		int calc = before*10;
-		if(calc > 0) calc += now;
-		else calc -= now;
-		recur(now+1, len, sum-before+calc, temp+" "+Integer.toString(now), calc); // " "
-	}
-	
+	public static StringBuilder sb = new StringBuilder();
+	public static int size;
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		int n = Integer.parseInt(br.readLine());
+		int n = Integer.parseInt(br.readLine()); // 0 < n < 10
 		
 		for(int i=0; i<n; i++) {
-			int num = Integer.parseInt(br.readLine());
-			String temp = "1";
-			recur(2, num, 1, temp, 1);
+			size = Integer.parseInt(br.readLine()); // 3 <= n <= 9
+			recur(1, new Stack<String>(), 1, 1);
 			sb.append("\n");
 		}
+		sb.deleteCharAt(sb.length()-1);
 		System.out.println(sb);
+	}
+
+	public static void recur(int num, Stack<String> list, int sum, int before) {
+		if(num == size) {
+			if(sum == 0) {
+				for(int i=0; i<list.size(); i++) {
+					sb.append(i+1);
+					sb.append(list.get(i));
+				}
+				sb.append(list.size()+1);
+				sb.append("\n");
+			}
+			return;
+		}
+		if(list.size() == 0) {
+			list.add(" ");
+			recur(num+1, list, sum-before+(before*10 + num+1), (before*10 + num+1));
+			list.pop();
+		} else {
+			String sign = list.peek();
+			list.add(" ");
+			if("+".equals(sign)) {
+				recur(num+1, list, sum-before+(before*10 + num+1), (before*10 + num+1));
+			} else if("-".equals(sign)) {
+				recur(num+1, list, sum-before+(before*10 - (num+1)), (before*10 + num+1) * -1);
+			} else if(" ".equals(sign)) {
+				recur(num+1, list, sum-before+(before*10 + num+1), (before*10 + num+1));
+			}
+			list.pop();
+		}
+		list.add("+");
+		recur(num+1, list, sum+num+1, num+1);
+		list.pop();
+		list.add("-");
+		recur(num+1, list, sum-(num+1), (num+1)*-1);
+		list.pop();
 	}
 }
